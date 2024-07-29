a696bdad017d85899636cbaf7bee27d8bc7dd693
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-29 11:20:55 -0400

    Update multistat-install.sh

--- multistat-install.sh
+++ multistat-install.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# wget -qO MDgui.sh 'raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat.sh' && sudo bash MDgui.sh
+## wget -qO - 'raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat.sh' | sudo -E bash
 wget https://raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat.sh
 chmod +x multistat.sh
 echo "************************************************"
