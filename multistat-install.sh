2c0ded7c3d00276d5dbb5daae629224bd62eaf68
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-29 11:16:34 -0400

    Create multistat-install.sh

---
+++ multistat-install.sh
@@ -0,0 +1,8 @@
+#!/bin/ssh
+wget https://raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat.sh
+chmod +x multistat.sh
+echo "************************************************"
+echo "Now edit password and ip addresses in the script"
+echo "************************************************"
+sleep 12
+nano multistat.sh
