6bdea242cb71107490f2cb234c1e0bca178622d9
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-29 11:24:43 -0400

    Update multistat-install.sh

--- multistat-install.sh
+++ multistat-install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # wget -qO MDgui.sh 'raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat.sh' && sudo bash MDgui.sh
-## wget -qO - 'raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat.sh' | sudo -E bash
+## wget -qO - 'raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat-install.sh' | sudo -E bash
 wget https://raw.githubusercontent.com/MachoDrone/Multi-Status-for-Nosana-Nodes/main/multistat.sh
 chmod +x multistat.sh
-echo "************************************************"
-echo "Now edit password and ip addresses in the script"
-echo "************************************************"
-sleep 12
-nano multistat.sh
+echo "******************************************************"
+echo "Now edit password and ip addresses in the script with: nano multistat.sh"
+echo "******************************************************"
